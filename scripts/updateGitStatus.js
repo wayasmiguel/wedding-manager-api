6743b8d9352de0c157c3eb68e2d799352068a902
wayasmiguel <wayasmiguel@outlook.com>
2021-06-06 03:34:52 -0500

    husky test

--- scripts/updateGitStatus.js
+++ scripts/updateGitStatus.js
@@ -1,7 +1,37 @@
 'use strict'
 
+
+const fs = require("fs");
+const path = require("path");
 const { execSync } = require("child_process");
 
-let commit = execSync("cat .git || git log -1 --pretty=%B");
 
-console.log(commit);
+const gitStatusPath = path.resolve('./gitStatus.json'); 
+let commit = execSync("git log -1 --pretty=%B").toString().trim();
+
+if(fs.existsSync(gitStatusPath)){
+    let gitStatus = JSON.parse(fs.readFileSync(gitStatusPath));
+    gitStatus.commit = commit;
+    fs.writeFileSync(gitStatusPath, 
+        JSON.stringify(gitStatus)
+    );
+}
+else{
+    fs.appendFileSync(gitStatusPath, 
+        JSON.stringify({
+            commit: commit
+        })
+    );
+}
+
+// exec("git log -1 --pretty=%B", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
