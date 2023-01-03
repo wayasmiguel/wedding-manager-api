c1e0d02da9bbce3587062e1e115cf799d9b74ec7
wayasmiguel <wayasmiguel@outlook.com>
2023-01-03 14:42:58 -0600

    initial commit

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ class DB{
                     this.destiny = `mongodb://${process.env.CLUSTER}/${process.env.DATABASE}`;
                 }
                 else if(process.env.CLUSTER !== "localhost:27017"){//localhost:27017
-                    this.destiny = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}${process.env.CLUSTER}/${process.env.DATABASE}?retryWrites=true&w=majority`;
+                    this.destiny = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@${process.env.CLUSTER}/${process.env.DATABASE}?retryWrites=true&w=majority`;
                 }
                 else{
                     this.destiny = `mongodb://${process.env.USER_DB}:${process.env.PASSWORD_DB}@${process.env.CLUSTER}/${process.env.DATABASE}`;
